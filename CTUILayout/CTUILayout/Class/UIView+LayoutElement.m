6bd2a49161094c0c5c877bf9327deda2147d3ecf
Chen tao <641066509@qq.com>
2015-02-04 13:49:29 +0800

    修改取值变化判断逻辑

--- CTUILayout/CTUILayout/Class/UIView+LayoutElement.m
+++ CTUILayout/CTUILayout/Class/UIView+LayoutElement.m
@@ -27,158 +27,178 @@ static const char INCLUDE_IN_LAYOUT;
  * left
  **/
 - (void)setLeft:(NSNumber *)left {
-    [self setLayoutWithPropertyName:(void *)&LEFT andValue:left];
+	[self setLayoutWithPropertyName:(void *)&LEFT andValue:left];
 }
 
 - (NSNumber *)left {
-    return objc_getAssociatedObject(self, &LEFT);
+	return objc_getAssociatedObject(self, &LEFT);
 }
 
 /**
  * right
  **/
 - (void)setRight:(NSNumber *)right {
-    [self setLayoutWithPropertyName:(void *)&RIGHT andValue:right];
+	[self setLayoutWithPropertyName:(void *)&RIGHT andValue:right];
 }
 
 - (NSNumber *)right {
-    return objc_getAssociatedObject(self, &RIGHT);
+	return objc_getAssociatedObject(self, &RIGHT);
 }
 
 /**
  * top
  **/
 - (void)setTop:(NSNumber *)top {
-    [self setLayoutWithPropertyName:(void *)&TOP andValue:top];
+	[self setLayoutWithPropertyName:(void *)&TOP andValue:top];
 }
 
 - (NSNumber *)top {
-    return objc_getAssociatedObject(self, &TOP);
+	return objc_getAssociatedObject(self, &TOP);
 }
 
 /**
  * bottom
  **/
 - (void)setBottom:(NSNumber *)bottom {
-    [self setLayoutWithPropertyName:(void *)&BOTTOM andValue:bottom];
+	[self setLayoutWithPropertyName:(void *)&BOTTOM andValue:bottom];
 }
 
 - (NSNumber *)bottom {
-    return objc_getAssociatedObject(self, &BOTTOM);
+	return objc_getAssociatedObject(self, &BOTTOM);
 }
 
 /**
  * verticalCenter
  **/
 - (void)setVerticalCenter:(NSNumber *)verticalCenter {
-    [self setLayoutWithPropertyName:(void *)&VERTICAL_CENTER andValue:verticalCenter];
+	[self setLayoutWithPropertyName:(void *)&VERTICAL_CENTER andValue:verticalCenter];
 }
 
 - (NSNumber *)verticalCenter {
-    return objc_getAssociatedObject(self, &VERTICAL_CENTER);
+	return objc_getAssociatedObject(self, &VERTICAL_CENTER);
 }
 
 /**
  * horizontalCenter
  **/
 - (void)setHorizontalCenter:(NSNumber *)horizontalCenter {
-    [self setLayoutWithPropertyName:(void *)&HORIZONTAL_CENTER andValue:horizontalCenter];
+	[self setLayoutWithPropertyName:(void *)&HORIZONTAL_CENTER andValue:horizontalCenter];
 }
 
 - (NSNumber *)horizontalCenter {
-    return objc_getAssociatedObject(self, &HORIZONTAL_CENTER);
+	return objc_getAssociatedObject(self, &HORIZONTAL_CENTER);
 }
 
 /**
  * percentHeight
  **/
 - (void)setPercentHeight:(NSNumber *)percentHeight {
-    [self setLayoutWithPropertyName:(void *)&PERCENT_HEIGHT andValue:percentHeight];
+	[self setLayoutWithPropertyName:(void *)&PERCENT_HEIGHT andValue:percentHeight];
 }
 
 - (NSNumber *)percentHeight {
-    return objc_getAssociatedObject(self, &PERCENT_HEIGHT);
+	return objc_getAssociatedObject(self, &PERCENT_HEIGHT);
 }
 
 /**
  * percentWidth
  **/
 - (void)setPercentWidth:(NSNumber *)percentWidth {
-    [self setLayoutWithPropertyName:(void *)&PERCENT_WIDTH andValue:percentWidth];
+	[self setLayoutWithPropertyName:(void *)&PERCENT_WIDTH andValue:percentWidth];
 }
 
 - (NSNumber *)percentWidth {
-    return objc_getAssociatedObject(self, &PERCENT_WIDTH);
+	return objc_getAssociatedObject(self, &PERCENT_WIDTH);
 }
 
 /**
  * includeInLayout
  **/
 - (void)setIncludeInLayout:(BOOL)includeInLayout {
-    NSNumber *oldValue = objc_getAssociatedObject(self, &INCLUDE_IN_LAYOUT);
-    if ([oldValue charValue] ^ includeInLayout) {
-        objc_setAssociatedObject(self, (void *)&INCLUDE_IN_LAYOUT, [NSNumber numberWithChar:includeInLayout], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        if (self.superview) {
-            [self.superview setNeedsLayout];
-        }
-    }
+	NSNumber *oldValue = objc_getAssociatedObject(self, &INCLUDE_IN_LAYOUT);
+	if ([oldValue charValue] ^ includeInLayout) {
+		objc_setAssociatedObject(self, (void *)&INCLUDE_IN_LAYOUT, [NSNumber numberWithChar:includeInLayout], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+		if (self.superview) {
+			[self.superview setNeedsLayout];
+		}
+	}
 }
 
 - (BOOL)includeInLayout {
-    NSNumber *value = objc_getAssociatedObject(self, &INCLUDE_IN_LAYOUT);
-    return [value charValue];
+	NSNumber *value = objc_getAssociatedObject(self, &INCLUDE_IN_LAYOUT);
+	return [value charValue];
 }
 
-- (void)setLayoutWithPropertyName:(void *)key andValue:(NSNumber *)value {
-    NSNumber *oldValue = objc_getAssociatedObject(self, key);
-    if ((nil == oldValue && nil != value) || (oldValue != value && [oldValue floatValue] != [value floatValue])) {
-        objc_setAssociatedObject(self, key, value, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        if (self.superview) {
-            [self.superview setNeedsLayout];
-        }
-    }
+- (void)setLayoutWithPropertyName:(void *)key andValue:(NSNumber *)newValue {
+	NSNumber *oldValue = objc_getAssociatedObject(self, key);
+
+	CGFloat oldValueFloatValue = [oldValue floatValue];
+	CGFloat newValueFloatValue = [newValue floatValue];
+
+	if (oldValue != newValue) {
+		if (nil == newValue || nil == oldValue) {
+			objc_setAssociatedObject(self, key, newValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+			if (self.superview) {
+				[self.superview setNeedsLayout];
+			}
+		}
+		else {
+			if (oldValueFloatValue != newValueFloatValue) {
+				objc_setAssociatedObject(self, key, newValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+				if (self.superview) {
+					[self.superview setNeedsLayout];
+				}
+			}
+		}
+	}
+	else {
+		if (oldValueFloatValue != newValueFloatValue) {
+			objc_setAssociatedObject(self, key, newValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+			if (self.superview) {
+				[self.superview setNeedsLayout];
+			}
+		}
+	}
 }
 
-
--(void)setX:(CGFloat)x{
-    CGRect newFrame=self.frame;
-    newFrame.origin.x=x;
-    self.frame=newFrame;
+- (void)setX:(CGFloat)x {
+	CGRect newFrame = self.frame;
+	newFrame.origin.x = x;
+	self.frame = newFrame;
 }
 
--(CGFloat)x{
-    return self.frame.origin.x;
+- (CGFloat)x {
+	return self.frame.origin.x;
 }
 
--(void)setY:(CGFloat)y{
-    CGRect newFrame=self.frame;
-    newFrame.origin.y=y;
-    self.frame=newFrame;
+- (void)setY:(CGFloat)y {
+	CGRect newFrame = self.frame;
+	newFrame.origin.y = y;
+	self.frame = newFrame;
 }
 
--(CGFloat)y{
-    return self.frame.origin.y;
+- (CGFloat)y {
+	return self.frame.origin.y;
 }
 
--(void)setWidth:(CGFloat)width{
-    CGRect newFrame=self.frame;
-    newFrame.size.width=width;
-    self.frame=newFrame;
+- (void)setWidth:(CGFloat)width {
+	CGRect newFrame = self.frame;
+	newFrame.size.width = width;
+	self.frame = newFrame;
 }
 
--(CGFloat)width{
-    return self.frame.size.width;
+- (CGFloat)width {
+	return self.frame.size.width;
 }
 
-
--(void)setHeight:(CGFloat)height{
-    CGRect newFrame=self.frame;
-    newFrame.size.height=height;
-    self.frame=newFrame;
+- (void)setHeight:(CGFloat)height {
+	CGRect newFrame = self.frame;
+	newFrame.size.height = height;
+	self.frame = newFrame;
 }
 
--(CGFloat)height{
-    return self.frame.size.height;
+- (CGFloat)height {
+	return self.frame.size.height;
 }
 
 @end
