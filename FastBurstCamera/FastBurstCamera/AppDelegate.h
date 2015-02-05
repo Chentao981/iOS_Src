ad94f276706b629fa7c0533c9fe9fe1d0a8d79f3
Chen tao <641066509@qq.com>
2015-02-05 10:58:02 +0800

    添加定制相机连拍项目

---
+++ FastBurstCamera/FastBurstCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastBurstCamera
+//
+//  Created by Chentao on 15/2/4.
+//  Copyright (c) 2015年 Chentao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
