4d7c6fab7fee9c18488e36d863eb1ff13cb7ea21
Chentao981 <641066509@qq.com>
2015-02-09 22:39:34 +0800

    绘制路径渐变

--- Mask_learn/Mask_learn/MainView.m
+++ Mask_learn/Mask_learn/MainView.m
@@ -25,11 +25,44 @@
         UIImage *photo2=[UIImage imageNamed:@"photo2.jpg"];
         circleView2.image=photo2;
         [self addSubview:circleView2];
-        
     }
     return self;
 }
 
 
+-(void)drawRect:(CGRect)rect{
+    // Create a gradient from white to red
+    CGFloat colors [] = {
+        1.0, 1.0, 1.0, 0.0,
+        1.0, 0.0, 0.0, 1.0
+    };
+    
+    CGColorSpaceRef baseSpace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(baseSpace, colors, NULL, 2);
+    CGColorSpaceRelease(baseSpace), baseSpace = NULL;
+    
+    CGContextRef context=UIGraphicsGetCurrentContext();
+
+    CGContextSetLineWidth(context, 10.0);
+    CGContextSetLineJoin(context, kCGLineJoinRound);
+    CGContextSetLineCap(context, kCGLineCapRound);
+    
+    
+    UIBezierPath *path=[UIBezierPath bezierPath];
+    [path addArcWithCenter:CGPointMake(100, 100) radius:95 startAngle:0 endAngle:2*M_PI clockwise:YES];
+    
+    [path moveToPoint:CGPointMake(300, 0)];
+    [path addLineToPoint:CGPointMake(300, 600)];
+    
+    CGContextAddPath(context, path.CGPath);
+    CGContextReplacePathWithStrokedPath(context);
+    CGContextClip(context);
+    
+    CGPoint startPoint = CGPointMake(0, 0);
+    CGPoint endPoint = CGPointMake(0, 200);
+    
+    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+    CGGradientRelease(gradient), gradient = NULL;
+}
 
 @end
